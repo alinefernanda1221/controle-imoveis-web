5850051682b13e26ca6f305b3cc1b5dae433fe85
Aline <alinefernanda1221@gmail.com>
2019-11-08 17:33:53 -0200

    Cadastrar Imóvel
    Cadastrar Imóvel
    - Incluída validação e limitação de campos do formulário;
    - Controller "limpa" para futura integração com rest.

--- src/modulos/imovel/cadastro/imovel-cadastro-controller.js
+++ src/modulos/imovel/cadastro/imovel-cadastro-controller.js
@@ -3,66 +3,30 @@ import { inherit } from "@uirouter/core";
 export default class ImovelCadastroController {
 
   constructor(ImovelService) {
-    var vm = this;
-    vm.cadastrarDocumento = null;
-    vm.adicionaPagina = null;
-    vm.paginas = null;
-    vm.status = null;
-    vm.limparTela = null;
-    vm.voltar = null;
+    let vm = this;
+    vm.voltar = voltar;
+    // vm.limparTela = limparTela;
+    vm.formIsValid = true;
+
     init();
 
-    
-    function init(){
-     /*inits*/
-     const endpoint = '/documento';
-     vm.paginas = [];
-     vm.adicionaPagina = adicionaPagina;
-     vm.excluirPagina = excluirPagina;
-     vm.limparTela = limparTela;
-     vm.voltar = voltar;
-     
-     vm.cadastrarDocumento = function (){
-    	 if(getPaginas().length > 0){
-    		 vm.documento.paginas = [];
-    		 vm.documento.paginas = getPaginas();    		 
-    	 }
-    	 
-    	 ImovelService.post(vm.documento, endpoint)
-    	 .then(function response(resp){
-    		 vm.status = 'Gravado com sucesso!';
-    		 alert(vm.status);
-             limparTela();
-             vm.paginas = [];
-    	 }).catch(function (error) {
-    		 vm.status = '';
-    		 vm.status = error.data.message;
-    		 alert(vm.status);
-    	  });
+    function init() {}
+
+    vm.cadastrarImovel = function(){
+      if(vm.formCadastroImovel.$valid){
+        vm.formIsValid = true;
+      }else{
+        vm.formIsValid = false
       }
-     
-     function adicionaPagina(pagina) { //Adiciona pagina num Array de Object
-		vm.paginas.push(angular.copy(pagina));
-     }
+    }
 
-     function excluirPagina(pagina) { //Remove pagina do Array
-    	 var index = vm.paginas.indexOf(pagina);
-    	 vm.paginas.splice(index, 1);
-     }
-     
-     function getPaginas(){
-    	 return vm.paginas;
-     }
-     
-     function limparTela() {
-		 document.getElementById("form-cadastro-imovel").reset(); // Reseta Form
-	 }
-     
-     function voltar() {
-    	  window.history.back();
-     };     
+    // function limparTela() {
+    //   document.getElementById("form-cadastro-imovel").reset(); // Reseta Form
+    // }
 
+    function voltar() {
+      window.history.back();
+    };
   }
- }
 }//Fim classe
 ImovelCadastroController.$inject = ['ImovelService'];
