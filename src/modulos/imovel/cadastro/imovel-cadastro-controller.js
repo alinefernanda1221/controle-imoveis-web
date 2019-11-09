cd72e439c3dca61fb593c66e44efd57eb4551ff6
Aline <alinefernanda1221@gmail.com>
2019-11-09 11:05:42 -0200

    Cadastrar Imóvel - Ajustes
    - Tamanho campos e ajustes de selects;
    - Limpar formulário.

--- src/modulos/imovel/cadastro/imovel-cadastro-controller.js
+++ src/modulos/imovel/cadastro/imovel-cadastro-controller.js
@@ -5,24 +5,48 @@ export default class ImovelCadastroController {
   constructor(ImovelService) {
     let vm = this;
     vm.voltar = voltar;
-    // vm.limparTela = limparTela;
+    vm.limpar = limpar;
     vm.formIsValid = true;
 
     init();
 
-    function init() {}
+    function init() {
+      vm.tiposImovel = [
+        { nome: 'CASA', valor: '1' },
+        { nome: 'APARTAMENTO', valor: '2' },
+        { nome: 'PRÉDIO', valor: '3' },
+        { nome: 'GALPÃO', valor: '4' },
+      ];
+
+      vm.situacoesImovel = [
+        { nome: 'DISPONÍVEL PARA LOCAÇÃO', valor: '1' },
+        { nome: 'EM CONSTRUÇÃO', valor: '2' }
+      ];
+    }
 
     vm.cadastrarImovel = function(){
+      console.log(vm.imovel);
       if(vm.formCadastroImovel.$valid){
         vm.formIsValid = true;
+        
+        ImovelService.cadastrarImovel()
+          .then((response) =>{
+            alert('Gravado com sucesso!');
+            vm.limpar();
+
+        }).catch((error) => {
+          alert('Ocorreu um erro!' + error.data);
+        });
       }else{
         vm.formIsValid = false
       }
     }
 
-    // function limparTela() {
-    //   document.getElementById("form-cadastro-imovel").reset(); // Reseta Form
-    // }
+    function limpar(){
+      vm.formCadastroImovel.$setUntouched();
+      document.getElementById("form-cadastro-imovel").reset(); // Reseta Form
+      vm.imovel = {};
+    }
 
     function voltar() {
       window.history.back();
