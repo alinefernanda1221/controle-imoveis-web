6b80043d6e6c94d36d6449d5933b0ea3382580c3
Aline <alinefernanda1221@gmail.com>
2019-11-18 00:53:22 -0200

    Imóvel módulo - Pesquisar
    - Adicionada view de pesquisa de imóveis.

--- src/modulos/imovel/pesquisa/imovel-pesquisa-controller.js
+++ src/modulos/imovel/pesquisa/imovel-pesquisa-controller.js
@@ -2,17 +2,55 @@ import { inherit } from "@uirouter/core";
 
 export default class ImovelPesquisaController {
 
-  constructor() {
+  constructor($uibModal, $state) {
     var vm = this;
     vm.voltar = voltar;
+    vm.formIsValid = true;
 
     init();
 
-    function init() {}
+    function init() {
+      vm.imoveisPesquisados = [
+        {
+          id: 1,
+          nome: 'Imovel Um',
+          endereco: { 
+            logradouro : 'QR 313 CONJUNTO 12 CASA 22',
+            estado: 'Distrito Federal - DF',
+            bairro: 'Samambaia Sul',
+            observacao: 'Observação XPTO'
+          },
+          tipo: { nome: 'CASA', valor: '1' },
+          quartos: 2,
+          descricao: 'Imovel XPTO'
+        },
+        {
+          id: 2,
+          nome: 'Imovel dois',
+          endereco: {
+            logradouro: 'QR 315 CONJUNTO 7 CASA 01',
+            estado: 'Gama - DF',
+            bairro: 'Ponte Alta Sul',
+            observacao: 'Observacao XYZ'
+          },
+          tipo: { nome: 'APARTAMENTO', valor: '2' },
+          quartos: 1,
+          descricao: 'Imovel XYZ'
+        }
+      ];
+
+    }
+
+    vm.editarImovel = function(imovelEditar) {
+      $state.go('imovel-edicao', {
+    		imovel : imovelEditar 
+    	 });
+
+    }
 
     function voltar() {
       window.history.back();
     };
   }
 }//Fim classe
-ImovelPesquisaController.$inject = [];
+ImovelPesquisaController.$inject = ['$uibModal', '$state'];
